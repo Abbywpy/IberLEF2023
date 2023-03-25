40de1d295711a5408e9f4445366da58367cb2f2b
Abby <peiyingabby@gmail.com>
2023-03-25 20:54:26 +0100

    add dataloader, clf into process, fix loss

--- trainer.py
+++ trainer.py
@@ -11,6 +11,9 @@ from pytorch_lightning import Trainer
 
 from models.lang_models.maria import MariaRoberta
 from models.lang_models.politibeto import PolitiBeto
+from models.clfs.simpleCLF import SimpleCLF
+
+from dataloader import SpanishTweetsDataModule
 
 from models.utils import concat_embeds
 from loss import cross_entropy_loss
@@ -21,12 +24,11 @@ class SpanishTweetsCLF(L.LightningModule):
         super().__init__()
         self.MariaRoberta = MariaRoberta()
         self.PolitiBeto = PolitiBeto()
-        self.clf = nn.Linear(768, 2)  # need to change this to a classifier
+        self.clf = SimpleCLF()  # need to change this to a classifier
         self.lr = lr
 
     def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
-        ret = {"data": x}
+        ret = {**x}
         ret |= self.MariaRoberta(**ret)
         ret |= self.PolitiBeto(**ret)
 
@@ -40,20 +42,32 @@ class SpanishTweetsCLF(L.LightningModule):
         ret = {**batch}
         ret |= self.MariaRoberta(**ret)
         ret |= self.PolitiBeto(**ret)
+        ret["concated_embeds"] = concat_embeds(**ret)
+
+        ret |= self.clf(**ret)
 
         # TODO: add accuracy, f1, etc.
-        loss = cross_entropy_loss(ret["result"], gt)
-        self.log("train_loss", loss)
+        loss = 0
+        # TODO: Add other categories loss
+        gender_loss = cross_entropy_loss(ret["pred_gender"], ret["gender"])
+        self.log("train_gender_loss", gender_loss)
+        loss += gender_loss
         return loss
 
     def validation_step(self, batch, batch_idx):
         ret = {**batch}
         ret |= self.MariaRoberta(**ret)
         ret |= self.PolitiBeto(**ret)
+        ret["concated_embeds"] = concat_embeds(**ret)
+
+        ret |= self.clf(**ret)
 
         # TODO: add accuracy, f1, etc.
-        loss = cross_entropy_loss(ret["result"], gt)
-        self.log("vaild_loss", loss)
+        loss = 0
+        # TODO: Add other categories loss
+        gender_loss = cross_entropy_loss(ret["pred_gender"], ret["gender"])
+        self.log("valid_gender_loss", gender_loss)
+        loss += gender_loss
         return loss
 
     def configure_optimizers(self):
