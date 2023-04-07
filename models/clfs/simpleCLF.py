62141dfdf5242ca9226eebfb812c52cbec872e50
miwytt <79340976+miwytt@users.noreply.github.com>
2023-04-07 19:13:11 +0200

    added dropout

--- models/clfs/simpleCLF.py
+++ models/clfs/simpleCLF.py
@@ -1,14 +1,4 @@
-# write a simple multi-layer binary classifier
-#  - input: 768
-#  - output: 2
-#  - hidden layers: 2
-#  - hidden layer size: 256
-#  - activation: ReLU
-#  - loss: CrossEntropyLoss
-#  - optimizer: Adam
-#  - learning rate: 1e-3
-#  - batch size: 32
-#  - epochs: 10
+# Simple classifier for each attribute
 
 # Path: models/clfs/simpleCLF.py
 import torch
@@ -17,7 +7,7 @@ import torch.nn.functional as F
 
 
 class SimpleCLF(nn.Module):
-    def __init__(self, attr_name, hidden_size=256, output_size=2, num_layers=2, bias=False):
+    def __init__(self, attr_name, hidden_size=256, output_size=2, num_layers=2, bias=False, dropout_rate=0.15):
         super().__init__()
         self.attr_name = attr_name
         self.hidden_size = hidden_size
@@ -27,10 +17,11 @@ class SimpleCLF(nn.Module):
 
         self.clf = nn.Sequential()
         for i in range(num_layers):
-            self.clf.add_module(f"fc{i}", nn.LazyLinear(hidden_size,bias))
+            self.clf.add_module(f"fc{i}", nn.LazyLinear(hidden_size, bias))
             self.clf.add_module(f"bn{i}", nn.BatchNorm1d(hidden_size))
             self.clf.add_module(f"relu{i}", nn.ReLU())
-        self.clf.add_module(f"lastlayer", nn.Linear(hidden_size,output_size))
+            self.clf.add_module(f"dropout{i}", nn.Dropout(dropout_rate))
+        self.clf.add_module(f"lastlayer", nn.Linear(hidden_size, output_size))
 
     def forward(self, concated_embeds, **kwargs):
         pred = self.clf(concated_embeds)
