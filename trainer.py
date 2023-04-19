2a18cfe1e2da9ccb3fe4c4cadbf4da500ffbccc0
miwytt <79340976+miwytt@users.noreply.github.com>
2023-04-19 16:13:09 +0200

    hparam and data set handling

--- trainer.py
+++ trainer.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
+import torchmetrics
 
 import lightning as L
 import lightning.pytorch as pl
@@ -40,6 +41,13 @@ class SpanishTweetsCLF(pl.LightningModule):
         self.MariaRoberta = MariaRoberta()
         self.PolitiBeto = PolitiBeto()
         self.TwitterXLM = TwitterXLM()
+        
+        # Add torchmetrics instances for precision, recall, and F1-score
+        self.metrics = {}
+        for attr in self.attr:
+            self.metrics[f"{attr}_precision"] = torchmetrics.Precision(num_classes=self.attr_size[self.attr.index(attr)], average='macro', task="multiclass")
+            self.metrics[f"{attr}_recall"] = torchmetrics.Recall(num_classes=self.attr_size[self.attr.index(attr)], average='macro', task="multiclass")
+            self.metrics[f"{attr}_f1"] = torchmetrics.classification.MulticlassF1Score(num_classes=self.attr_size[self.attr.index(attr)], average='macro', task="multiclass")
 
         # TODO: finetune SimpleCLF classifier
         if clf == "simple":
@@ -69,6 +77,7 @@ class SpanishTweetsCLF(pl.LightningModule):
         ret.update(self.TwitterXLM(**ret))
 
         ret["concated_embeds"] = concat_embeds(**ret)
+        
 
         for attr in self.attr:
             ret.update(getattr(self, f'clf_{attr}')(**ret))
@@ -107,16 +116,23 @@ class SpanishTweetsCLF(pl.LightningModule):
         for attr in self.attr:
             ret.update(getattr(self, f'clf_{attr}')(**ret))
 
-        # TODO: add f1, etc.
         loss = 0
         for attr in self.attr:
             attr_loss = cross_entropy_loss(ret[f"pred_{attr}"], ret[attr])
-
+            loss += attr_loss
+            
+            # Calculate and log precision, recall, and F1-score
+            precision = self.metrics[f"{attr}_precision"](ret[f"pred_{attr}"], ret[attr])
+            recall = self.metrics[f"{attr}_recall"](ret[f"pred_{attr}"], ret[attr])
+            f1 = self.metrics[f"{attr}_f1"](ret[f"pred_{attr}"], ret[attr])
+            
             self.log(f"valid_{attr}_loss", attr_loss)
             self.log(f"valid_{attr}_acc", accuracy(
                 ret[f"pred_{attr}"], ret[attr]))
+            self.log(f"valid_{attr}_precision", precision)
+            self.log(f"valid_{attr}_recall", recall)
+            self.log(f"valid_{attr}_f1", f1)
 
-            loss += attr_loss
         return loss
 
     def configure_optimizers(self):
@@ -126,20 +142,32 @@ class SpanishTweetsCLF(pl.LightningModule):
 
 def main(hparams):
     if hparams.clf == "simple":
-        with open('simpleCLF_config.yaml') as f:
+        with open('best_hyperparams_simpleCLF.yaml') as f:
             default_config = yaml.load(f, Loader=yaml.FullLoader)
+            
+            # TODO: after hparam search remove this model and uncomment the model, epochs and batch_size below
+            model = SpanishTweetsCLF(clf="simple", freeze_lang_model=True, lr=1e-3, dropout_rate=0.2, hidden_size=128, num_layers=2, bias=False)
+
+            
+            #model = SpanishTweetsCLF(clf="simple", freeze_lang_model=True, lr=default_config["lr", dropout_rate=default_config["dropout"], hidden_size=default_config["hidden_size"], num_layers=default_config["num_layers"], bias=False)
+            
+            #epochs = default_config["epochs"]
+            #batch_size = default_config["batch_size"]
     else:
         raise NotImplementedError
-
-    model = SpanishTweetsCLF(**default_config["model"])
-
-    # TODO: change train_dataset_path and val_dataset_path
+    
+    # TODO: after hparam search the batch_size from default_config can be used
     if hparams.tiny_train:
         dm = SpanishTweetsDataModule(
-            train_dataset_path="data/practise_data/cleaned/cleaned_encoded_development_train.csv",
-            val_dataset_path="data/practise_data/cleaned/cleaned_encoded_development_test.csv",
+            train_dataset_path="data/tiny_data/tiny_cleaned_encoded_train.csv", # path leads to *very* small subset of practise data
+            val_dataset_path="data/tiny_data/tiny_cleaned_encoded_development.csv", # path leads to *very* small subset of practise data
             batch_size=hparams.batch_size)
         print("Using tiny train")
+    elif hparams.practise:
+        dm = SpanishTweetsDataModule(
+            train_dataset_path="data/practise_data/cleaned/cleaned_encoded_development_train.csv", # path leads to  practise data
+            val_dataset_path="data/practise_data/cleaned/cleaned_encoded_development_test.csv", # path leads to practise data
+            batch_size=hparams.batch_size)
     else:
         dm = SpanishTweetsDataModule(train_dataset_path="data/full_data/cleaned/train_clean_encoded.csv",
                                      val_dataset_path="data/full_data/cleaned/val_clean_encoded.csv",
@@ -148,6 +176,7 @@ def main(hparams):
         print("Using full train")
 
     wandb_logger = WandbLogger(project="spanish-tweets")
+    # TODO: after hparam search the epochs from default_config can be used
     trainer = L.Trainer(accelerator=hparams.accelerator, logger=wandb_logger, max_epochs=hparams.epochs)
     trainer.fit(model, dm)
 
@@ -160,6 +189,7 @@ if __name__ == '__main__':
     parser.add_argument("--epochs", "-e", type=int, default=3, help="Number of epochs to train (default: 3)")
     parser.add_argument("--clf", "-c", type=str, default="simple", help="Classifier to use (default: simple)")
     parser.add_argument("--tiny_train", "-tiny", action="store_true", help="Use tiny train dataset (default: False)")
+    parser.add_argument("--practise_train", "-practise", action="store_true", help="Use tiny train dataset (default: False)")
     args = parser.parse_args()
 
     main(args)
