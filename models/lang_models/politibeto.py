bcf7a1b4e6e2197e557ea129630b793c82c2d1e6
Abby <peiyingabby@gmail.com>
2023-04-20 14:15:12 +0200

    fix device

--- models/lang_models/politibeto.py
+++ models/lang_models/politibeto.py
@@ -12,13 +12,6 @@ class PolitiBeto(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name)
 
-    def get_embeddings(self, device, text_batch):
-        inputs = self.tokenizer(
-            text_batch, return_tensors="pt", padding=True, truncation=True, max_length=128).to(device)
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-        return outputs[0]
-
     def get_sentence_embeddings(self, embeddings):
         # Calculate the mean of the token embeddings along the sequence dimension (dim=1)
         sentence_embeddings = embeddings.mean(dim=1)
@@ -29,9 +22,15 @@ class PolitiBeto(nn.Module):
         normalized_embeddings = embeddings.div(norms)
         return normalized_embeddings
 
-    def forward(self, tweet, **kwargs):
-        token_embeddings = self.get_embeddings(tweet)
-        sentence_embeddings = self.get_sentence_embeddings(token_embeddings)
+    def forward(self, tweet, device, **kwargs):
+
+        encoded_input = self.tokenizer(
+            tweet, padding=True, truncation=True, max_length=128, return_tensors='pt').to(device)
+
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+
+        sentence_embeddings = self.get_sentence_embeddings(model_output[0])
         normalized_embeddings = self.normalize_embeddings(sentence_embeddings)
         return {"politibeto_embed": normalized_embeddings, **kwargs}
     
