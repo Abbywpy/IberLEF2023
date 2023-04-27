31f15f4558afe07e882a701575ce06af00b02e57
miwytt <79340976+miwytt@users.noreply.github.com>
2023-04-27 20:53:01 +0200

    put save last to false

--- trainer.py
+++ trainer.py
@@ -239,7 +239,7 @@ def main(hparams):
 
     trainer = L.Trainer(callbacks=[
                         EarlyStopping(monitor="valid/average_final_metric", mode="max", patience=3),
-                        ModelCheckpoint(monitor="valid/average_final_metric", mode="max", save_top_k=3, save_last=True, verbose=True, filename="{epoch}-{valid/average_final_metric:.2f}"),
+                        ModelCheckpoint(monitor="valid/average_final_metric", mode="max", save_top_k=3, save_last=False, verbose=True, filename="{epoch}-{valid/average_final_metric:.2f}"),
                         ModelCheckpoint(monitor="valid/profession/final_metric", mode="max", save_top_k=1, save_last=False, verbose=True, filename="{epoch}-{valid/profession/final_metric:.2f}"),
                         ModelCheckpoint(monitor="valid/gender/final_metric", mode="max", save_top_k=1, save_last=False, verbose=True, filename="{epoch}-{valid/gender/final_metric:.2f}"),
                         ModelCheckpoint(monitor="valid/ideology_multiclass/final_metric", mode="max", save_top_k=1, save_last=False, verbose=True, filename="{epoch}-{valid/ideology_multiclass/final_metric:.2f}"),
